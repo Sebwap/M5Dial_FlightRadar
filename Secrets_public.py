dc0c0d367afa111a98c55a7639c322d55f398570
Sebwap <53824549+Sebwap@users.noreply.github.com>
2024-02-19 21:28:30 +0100

    Add files via upload

---
+++ Secrets_public.py
@@ -0,0 +1,10 @@
+# WIFI
+WIFI_SSID = "Your SSID"
+WIFI_PASSWORD = "WIFI PASSWORD"
+
+# Location
+LAT_DOM= 1.111111 # your latitude
+LON_DOM= 2.222222 # your longitude
+
+# rayon ADS-B Flightradar
+RAYON_PLANE=20000 # viewing distance, in meter
